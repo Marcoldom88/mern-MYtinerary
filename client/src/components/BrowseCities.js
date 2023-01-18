2c8dce2a4b7120e6c0bfa46923c2bfeb2969a31d
Marcoldom88 <marcoldom88@gmail.com>
2023-01-18 17:54:57 +0100

    implemented Redux for fetching data from database

--- client/src/components/BrowseCities.js
+++ client/src/components/BrowseCities.js
@@ -1,19 +1,27 @@
 import CreateCitiesCarousel from "./CreateCitiesCarousel";
-import { useState, useEffect } from "react";
+import { useGetCitiesQuery } from "../store/features/apiSlice";
 
 const BrowseCities = () => {
- const [cities, setCities] = useState();
-  const url = "http://localhost:5000/all";
-  useEffect(() => {
-    const fetchCities = async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setCities(json);
-    }
-    fetchCities();
-  }, []);
+ const {
+    data: cities,
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  } = useGetCitiesQuery()
 
+  let content
+
+  if (isLoading) {
+    content =
+      <div className="spinner-border text-success" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+  } else if (isSuccess) {
+    content = <CreateCitiesCarousel cities = {cities} />
+  } else if (isError) {
+    content = <div>{error.toString()}</div>
+  }
 
   if (!cities) return
     <div className="spinner-border text-success" role="status">
@@ -22,7 +30,7 @@ const BrowseCities = () => {
 
   return (
     <>
-      <CreateCitiesCarousel cities = {cities} />
+      {content}
     </>
     
   )
--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -4,6 +4,7 @@ import "../stylesheets/CreateCitiesCarousel.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {  faChevronLeft,  faChevronRight,} from "@fortawesome/free-solid-svg-icons";
 
+
 const Carousel = ({data}) => {
     const [index, setIndex] = useState(0);
      //slide control for carousel
